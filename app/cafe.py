038877794fa4db7f488115a6e64fcf409bcdfefb
Your Name <stas101203rakitin@gmail.com>
2025-02-06 11:44:09 +0200

    Solution

--- app/cafe.py
+++ app/cafe.py
@@ -11,12 +11,12 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         """Checks if a visitor meets the pandemic restrictions."""
         if "vaccine" not in visitor:
-            raise NotVaccinatedError("Visitor invaccine error")
+            raise NotVaccinatedError()
 
         if visitor["vaccine"]["expiration_date"] < datetime.date.today():
-            raise OutdatedVaccineError("Visitor's invaccine outdated error")
+            raise OutdatedVaccineError()
 
         if not visitor.get("wearing_a_mask", False):
-            raise NotWearingMaskError("Mask wearing error")
+            raise NotWearingMaskError()
 
         return f"Welcome to {self.name}"
